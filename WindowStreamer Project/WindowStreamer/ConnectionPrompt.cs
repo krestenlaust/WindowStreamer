7fd845114df4a3824bff31d97a3c206f187cfc06
kres0345 <kressckerl@gmail.com>
2019-05-16 19:42:09 +0200

    Globalization preperations

--- WindowStreamer Project/WindowStreamer/ConnectionPrompt.cs
+++ WindowStreamer Project/WindowStreamer/ConnectionPrompt.cs
@@ -6,7 +6,9 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 using System.Windows.Forms;
+using System.Globalization;
 
 namespace Server
 {
