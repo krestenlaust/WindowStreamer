65ca16c34906e2ab28231be50ae500e33cb5f89f
Kresten Laust <krestenlaust@gmail.com>
2023-03-11 22:55:35 +0100

    Finished translating the client application and corrected a mistake with mnemonic keys in english.

--- Client/ConnectWindow.Designer.cs
+++ Client/ConnectWindow.Designer.cs
@@ -28,98 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBoxTargetIPAddress = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.textBoxTargetPort = new System.Windows.Forms.TextBox();
-            this.buttonConnect = new System.Windows.Forms.Button();
-            this.buttonCancel = new System.Windows.Forms.Button();
-            this.SuspendLayout();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ConnectWindow));
+            textBoxTargetIPAddress = new System.Windows.Forms.TextBox();
+            label1 = new System.Windows.Forms.Label();
+            label2 = new System.Windows.Forms.Label();
+            textBoxTargetPort = new System.Windows.Forms.TextBox();
+            buttonConnect = new System.Windows.Forms.Button();
+            buttonCancel = new System.Windows.Forms.Button();
+            SuspendLayout();
             // 
             // textBoxTargetIPAddress
             // 
-            this.textBoxTargetIPAddress.Location = new System.Drawing.Point(44, 67);
-            this.textBoxTargetIPAddress.Margin = new System.Windows.Forms.Padding(2);
-            this.textBoxTargetIPAddress.Name = "textBoxTargetIPAddress";
-            this.textBoxTargetIPAddress.Size = new System.Drawing.Size(88, 23);
-            this.textBoxTargetIPAddress.TabIndex = 0;
+            resources.ApplyResources(textBoxTargetIPAddress, "textBoxTargetIPAddress");
+            textBoxTargetIPAddress.Name = "textBoxTargetIPAddress";
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(41, 47);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(97, 15);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Target IP Address";
+            resources.ApplyResources(label1, "label1");
+            label1.Name = "label1";
             // 
             // label2
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(44, 103);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(64, 15);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "Target port";
+            resources.ApplyResources(label2, "label2");
+            label2.Name = "label2";
             // 
             // textBoxTargetPort
             // 
-            this.textBoxTargetPort.Location = new System.Drawing.Point(44, 121);
-            this.textBoxTargetPort.Margin = new System.Windows.Forms.Padding(2);
-            this.textBoxTargetPort.Name = "textBoxTargetPort";
-            this.textBoxTargetPort.Size = new System.Drawing.Size(88, 23);
-            this.textBoxTargetPort.TabIndex = 1;
+            resources.ApplyResources(textBoxTargetPort, "textBoxTargetPort");
+            textBoxTargetPort.Name = "textBoxTargetPort";
             // 
             // buttonConnect
             // 
-            this.buttonConnect.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
-            this.buttonConnect.Location = new System.Drawing.Point(222, 47);
-            this.buttonConnect.Margin = new System.Windows.Forms.Padding(2);
-            this.buttonConnect.Name = "buttonConnect";
-            this.buttonConnect.Size = new System.Drawing.Size(131, 93);
-            this.buttonConnect.TabIndex = 2;
-            this.buttonConnect.Text = "&Connect";
-            this.buttonConnect.UseVisualStyleBackColor = false;
-            this.buttonConnect.Click += new System.EventHandler(this.buttonConnect_Click);
+            resources.ApplyResources(buttonConnect, "buttonConnect");
+            buttonConnect.BackColor = System.Drawing.Color.FromArgb(128, 128, 255);
+            buttonConnect.Name = "buttonConnect";
+            buttonConnect.UseVisualStyleBackColor = false;
+            buttonConnect.Click += buttonConnect_Click;
             // 
             // buttonCancel
             // 
-            this.buttonCancel.BackColor = System.Drawing.Color.Red;
-            this.buttonCancel.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.buttonCancel.Location = new System.Drawing.Point(222, 148);
-            this.buttonCancel.Margin = new System.Windows.Forms.Padding(2);
-            this.buttonCancel.Name = "buttonCancel";
-            this.buttonCancel.Size = new System.Drawing.Size(131, 27);
-            this.buttonCancel.TabIndex = 3;
-            this.buttonCancel.Text = "&Cancel";
-            this.buttonCancel.UseVisualStyleBackColor = false;
-            this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
+            resources.ApplyResources(buttonCancel, "buttonCancel");
+            buttonCancel.BackColor = System.Drawing.Color.Red;
+            buttonCancel.ForeColor = System.Drawing.SystemColors.ControlText;
+            buttonCancel.Name = "buttonCancel";
+            buttonCancel.UseVisualStyleBackColor = false;
+            buttonCancel.Click += buttonCancel_Click;
             // 
             // ConnectWindow
             // 
-            this.AcceptButton = this.buttonConnect;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.CancelButton = this.buttonCancel;
-            this.ClientSize = new System.Drawing.Size(402, 219);
-            this.Controls.Add(this.buttonCancel);
-            this.Controls.Add(this.buttonConnect);
-            this.Controls.Add(this.textBoxTargetPort);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBoxTargetIPAddress);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Margin = new System.Windows.Forms.Padding(2);
-            this.MaximizeBox = false;
-            this.Name = "ConnectWindow";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
-            this.Text = "Connect to server";
-            this.Load += new System.EventHandler(this.ConnectWindow_Load);
-            this.ResumeLayout(false);
-            this.PerformLayout();
-
+            AcceptButton = buttonConnect;
+            resources.ApplyResources(this, "$this");
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            CancelButton = buttonCancel;
+            Controls.Add(buttonCancel);
+            Controls.Add(buttonConnect);
+            Controls.Add(textBoxTargetPort);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(textBoxTargetIPAddress);
+            FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            Name = "ConnectWindow";
+            Load += ConnectWindow_Load;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
