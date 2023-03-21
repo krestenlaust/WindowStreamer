f040584b4477150f25b23677e25aa9edf445e8b5
Kresten Laust <krestenlaust@gmail.com>
2023-03-21 20:04:58 +0100

    - Removed unused native function calls - Removed localization helper methods, because the forms have been localized in a different way.

--- Protocol/Helper.cs
+++ Protocol/Helper.cs
@@ -16,35 +16,5 @@ namespace Protocol
         /// Returns unix timestamp.
         /// </summary>
         public static long TimeStamp() => DateTimeOffset.Now.ToUnixTimeMilliseconds();
-
-        public static string LocalizeParameter(string localizedString, object parameter0)
-        {
-            var sb = new StringBuilder(localizedString);
-
-            sb.Replace("%0", parameter0.ToString());
-
-            return sb.ToString();
-        }
-
-        public static string LocalizeParameter(string localizedString, object parameter0, object parameter1)
-        {
-            var sb = new StringBuilder(localizedString);
-
-            sb.Replace("%0", parameter0.ToString());
-            sb.Replace("%1", parameter1.ToString());
-
-            return sb.ToString();
-        }
-
-        public static string LocalizeParameter(string localizedString, object parameter0, object parameter1, object parameter2)
-        {
-            var sb = new StringBuilder(localizedString);
-
-            sb.Replace("%0", parameter0.ToString());
-            sb.Replace("%1", parameter1.ToString());
-            sb.Replace("%2", parameter2.ToString());
-
-            return sb.ToString();
-        }
     }
 }
