226efac03906955280788c997b2b65b5f5dfa7a2
Kresten Laust <krestenlaust@gmail.com>
2023-07-04 17:44:16 +0200

    Moved exception into separate folder

--- WindowStreamer.Server/WindowServer.cs
+++ WindowStreamer.Server/WindowServer.cs
@@ -1,22 +1,6 @@
-﻿using System.Diagnostics;
-using System.Drawing;
-using System.Drawing.Imaging;
-using System.Net;
-using System.Net.Sockets;
-using System.Runtime.InteropServices;
-using System.Text;
-using Serilog;
-using WindowStreamer.Protocol;
+﻿using WindowStreamer.Server.Exceptions;
 
 namespace WindowStreamer.Server;
-
-public class InstanceAlreadyInUseException : Exception
-{
-    public InstanceAlreadyInUseException(string msg)
-        : base(msg)
-    {
-    }
-}
 
 public partial class WindowServer : IDisposable
 {
