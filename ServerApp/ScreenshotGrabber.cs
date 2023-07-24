69079d536380209efab693e147129f0414c26623
Kresten Laust <krestenlaust@gmail.com>
2023-07-24 16:51:17 +0200

    Resolved crash on UAC prompt during stream

--- ServerApp/ScreenshotGrabber.cs
+++ ServerApp/ScreenshotGrabber.cs
@@ -6,6 +6,7 @@ namespace ServerApp;
 
 internal class ScreenshotGrabber : IScreenshotQuery
 {
+    static readonly Brush UacPromptFillColor = Brushes.Turquoise;
     readonly IGetCaptureArea captureArea;
 
     public ScreenshotGrabber(IGetCaptureArea captureArea)
@@ -20,7 +21,15 @@ internal class ScreenshotGrabber : IScreenshotQuery
         var bmp = new Bitmap(captureRect.Width, captureRect.Height, PixelFormat.Format24bppRgb);
 
         Graphics g = Graphics.FromImage(bmp);
-        g.CopyFromScreen(captureRect.Left, captureRect.Top, 0, 0, bmp.Size, CopyPixelOperation.SourceCopy);
+        try
+        {
+            // Exception happens during UAC prompts.
+            g.CopyFromScreen(captureRect.Left, captureRect.Top, 0, 0, bmp.Size, CopyPixelOperation.SourceCopy);
+        }
+        catch (System.ComponentModel.Win32Exception)
+        {
+            g.FillRectangle(UacPromptFillColor, captureRect);
+        }
 
         return bmp;
     }
