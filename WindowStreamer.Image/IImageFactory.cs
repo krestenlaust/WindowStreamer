912ca9422ce4fc382ad00b16361694214c78849f
Kresten Laust <krestenlaust@gmail.com>
2023-08-04 15:21:01 +0200

    Added IImageFactory and implementations for IImage and IImageFactory

---
+++ WindowStreamer.Image/IImageFactory.cs
@@ -0,0 +1,13 @@
+﻿namespace WindowStreamer.Image
+{
+    public interface IImageFactory
+    {
+        /// <summary>
+        /// Create an image by image data and dimensions.
+        /// </summary>
+        /// <param name="imageData">24-bit RGB pixel data.</param>
+        /// <param name="dimensions">The dimensions of the image.</param>
+        /// <returns></returns>
+        IImage CreateImage(byte[] imageData, Size dimensions);
+    }
+}
