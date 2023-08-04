01307506e7873f0494ea1fe8a2b2acdc1c2d8458
Kresten Laust <krestenlaust@gmail.com>
2023-08-04 14:09:49 +0200

    Added some of the types and classes needed to abstract Windows-dependencies

---
+++ WindowStreamer.Image/Point.cs
@@ -0,0 +1,14 @@
+﻿namespace WindowStreamer.Image
+{
+    public readonly struct Point
+    {
+        public readonly int X;
+        public readonly int Y;
+
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
